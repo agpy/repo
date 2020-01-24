fd97317a4ceedc0bb02795853ff476ba99cad198
agpy <agp73@mail.ru>
2020-01-24 21:06:25 +0300

    Add all changes to commit

--- 04_data_structures/task_4_1.py
+++ 04_data_structures/task_4_1.py
@@ -7,7 +7,6 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
-'''
 
 NAT = 'ip nat inside source list ACL interface FastEthernet0/1 overload'
 
@@ -15,3 +14,12 @@ NAT = NAT.replace('FastEthernet', 'GigabitEthernet')
 lng = len(NAT)
 
 print('#'*lng,'\n' + NAT,'\n' + '#'*lng)
+'''
+
+#vlan, mac, intf = ['100', '000.000.000', 'FastEthernet0/24']
+#oct1, oct2, oct3, oct4 = (192, 168, 0, 1)
+ip_list = ['192.168.0.1/24', '172.16.0.1/12', '10.0.0.1/8']
+
+for ip_addr in ip_list:
+    ip, mask = ip_addr.split('/')
+    print(f"IP: {ip} mask: {mask}")
